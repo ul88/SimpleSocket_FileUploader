c06459f8301f72167b8d6ac6dc8e343998ac0890
ul88 <hkimog13@naver.com>
2024-10-16 02:13:26 +0900

    초기 커밋.

---
+++ client/clientMain.cpp
@@ -0,0 +1,202 @@
+#include<filesystem>
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<thread>
+#include<winsock2.h>
+#include<ws2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+#define SERVERIP "127.0.0.1"
+#define BUFSIZE 1024
+using namespace std;
+
+const char* directoryPath = "download_file\\";
+
+void recv_file(SOCKET sock) {
+	char buf[BUFSIZE];
+	recv(sock, buf, BUFSIZE, 0);
+	send(sock, "1", 1, 0);
+	string file_name = buf;
+
+	recv(sock, buf, BUFSIZE, 0);
+	send(sock, "1", 1, 0);
+
+	string temp = buf;
+	int file_size = stoi(temp);
+
+	char* file_data = new char[file_size];
+	recv(sock, file_data, file_size, 0);
+	send(sock, "1", 1, 0);
+
+	ofstream out(directoryPath + file_name, ios::out | ios::binary);
+
+	out.write(file_data, file_size);
+	cout << file_name << " 다운로드 성공" << endl;
+
+	out.close();
+	delete[] file_data;
+}
+
+void send_file(SOCKET sock, string path) {
+	char sign;
+	ifstream is(path, ios::in | ios::binary);
+	if (!is) {
+		cout << "파일 열기 오류"<<endl;
+		is.close();
+		return;
+	}
+
+	//파일명을 서버로 전송
+	send(sock, path.c_str(), strlen(path.c_str())+1, 0);
+
+	is.seekg(0, is.end);
+	int file_size = is.tellg();
+	is.seekg(0, is.beg);
+
+	send(sock, to_string(file_size).c_str(), strlen(to_string(file_size).c_str()), 0);
+	
+	//파일 크기 전송 완료 신호 보냄
+	recv(sock, &sign, 1, 0);
+	cout << "파일 크기 전송 완료 [" << file_size <<"]" << endl;
+
+	char* file_data = new char[file_size];
+	is.read(file_data, file_size);
+
+	send(sock, (const char*)file_data, file_size, 0);
+
+	recv(sock, &sign, 1, 0);
+	cout << "서버로 파일 전송 성공"<<endl;
+
+	is.close();
+	delete[] file_data;
+}
+
+void recv_file_list(SOCKET sock) {
+	char buf[1024];
+	char sign;
+
+	int len = 0;
+	recv(sock, buf, 1024, 0);
+	send(sock, "1", 1, 0);
+
+	len = atoi(buf);
+	for (int i = 0;i < len;i++) {
+		memset(buf, 0, sizeof(buf));
+		recv(sock, buf, 1024, 0);
+		send(sock, "1", 1, 0);
+		cout << buf << endl;
+	}
+}
+
+void client_file_list() {
+	const char* direct = "download_file";
+	int idx = 0;
+	for (auto iter : filesystem::directory_iterator(direct)) {
+		string p = iter.path().generic_string();
+		p.erase(0, p.find("/") + 1);
+		cout << ++idx << " " << p << endl;
+	}
+}
+
+void client_file_delete() {
+	const char* direct = "download_file";
+	
+	client_file_list();
+	
+	int num;
+	cout << "삭제하고 싶은 파일을 선택해주세요: ";
+	cin >> num;
+
+	int idx = 0;
+	for (auto iter : filesystem::directory_iterator(direct)) {
+		if (++idx == num) {
+			string p = iter.path().generic_string();
+			if (::remove(p.c_str()) == 0) {
+				cout << "파일 삭제에 성공했습니다." << endl;
+			}
+			else {
+				cout << "파일 삭제에 실패했습니다." << endl;
+			}
+			break;
+		}
+	}
+}
+
+int main()
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+	SOCKET sock{ socket(AF_INET, SOCK_STREAM, 0) };
+	sockaddr_in addr{ AF_INET, htons(4444) };
+	inet_pton(AF_INET, SERVERIP, &addr.sin_addr);
+	if (connect(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) return 1;
+
+	bool flag = true;
+	char sign;
+	while (flag) {
+		int command, num;
+		string path,temp;
+		cout << "1. 파일 업로드" << endl;
+		cout << "2. 파일 다운로드" << endl;
+		cout << "3. 서버에 존재하는 파일 확인" << endl;
+		cout << "4. 서버에 파일 삭제" << endl;
+		cout << "5. 클라이언에 존재하는 파일 확인" << endl;
+		cout << "6. 클라이언트에 파일 삭제" << endl;
+		cout << "7. 클라이언트 종료" << endl;
+		cin >> command;
+		cin.ignore();
+
+		switch (command) {
+		case 1:
+			cout << "전송할 파일의 위치를 적어주세요." << endl;
+			getline(cin, path);
+			//경로에서 "" 삭제
+			while (path.find("\"") != string::npos)
+				path.erase(find(path.begin(), path.end(), '\"'));
+
+			// 서버에 1을 전송
+			send(sock, "1", 1, 0);
+			
+			//파일을 서버에 전송
+			send_file(sock, path);
+			break;
+		case 2:
+			send(sock, "2", 1, 0);
+			recv_file_list(sock);
+			cout << "다운로드하고 싶은 파일을 선택해주세요: ";
+			cin >> num;
+			send(sock, to_string(num).c_str(), to_string(num).length() + 1, 0);
+			recv(sock, &sign, 1, 0);
+			recv_file(sock);
+			break;
+		case 3:
+			send(sock, "3", 1, 0);
+			recv_file_list(sock);
+			break;
+		case 4:
+			send(sock, "4", 1, 0);
+			recv_file_list(sock);
+			cout << "삭제하고 싶은 파일을 선택해주세요: ";
+			cin >> num;
+			temp = to_string(num);
+			send(sock, temp.c_str(), temp.length() + 1, 0);
+			recv(sock, &sign, 1, 0);
+			break;
+		case 5:
+			client_file_list();
+			break;
+		case 6:
+			client_file_delete();
+			break;
+		case 7:
+			flag = false;
+			send(sock, "7", 1, 0);
+			break;
+		}
+	}
+	
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
---
+++ server/serverMain.cpp
@@ -0,0 +1,261 @@
+#include<filesystem>
+#include<fstream>
+#include<iostream>
+#include<winsock2.h>
+#include<ws2tcpip.h>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<thread>
+#include<io.h>
+#pragma comment(lib,"ws2_32.lib")
+
+#define BUFSIZE 1024
+
+using namespace std;
+
+const char* directoryPath = "download_file\\";
+vector<string> paths;
+
+void init_paths() {
+
+	const char* direct = "download_file";
+
+	for (auto iter : filesystem::directory_iterator(direct)) {
+		string p = iter.path().generic_string();
+		p.erase(0, p.find("/")+1);
+		paths.push_back(p);
+	}
+}
+
+void send_file(SOCKET client_sock, int idx, sockaddr_in clientaddr) {
+	char sign;
+	ifstream is(directoryPath + paths[idx - 1], ios::in | ios::binary);
+	if (!is) {
+		cout << "파일 열기 오류";
+		return;
+	}
+
+	send(client_sock, paths[idx - 1].c_str(), paths[idx - 1].length() + 1, 0);
+	recv(client_sock, &sign, 1, 0);
+
+	is.seekg(0, is.end);
+	int file_size = is.tellg();
+	is.seekg(0, is.beg);
+
+	send(client_sock, to_string(file_size).c_str(), 
+		to_string(file_size).length() + 1, 0);
+	recv(client_sock, &sign, 1, 0);
+
+	char* file_data = new char[file_size];
+	is.read(file_data, file_size);
+	send(client_sock, file_data, file_size, 0);
+	recv(client_sock, &sign, 1, 0);
+	
+	char addr[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+	cout << "[TCP 서버] "<< addr << ":" <<
+		ntohs(clientaddr.sin_port) << "로 " << 
+		paths[idx - 1] << "를 전송했습니다." << endl;
+
+	is.close();
+	delete[] file_data;
+}
+
+void recv_file_clientToServer(SOCKET client_sock, sockaddr_in clientaddr) {
+	char buf[BUFSIZE];
+	int ret;
+	ret = recv(client_sock, buf, BUFSIZE, 0);
+	if (ret == SOCKET_ERROR || ret == 0) return;
+
+	//디렉토리를 전부 자르고 파일명만 남김
+	string temp = buf;
+	istringstream ss(temp);
+	string path;
+	while (getline(ss, path, '\\'));
+	paths.push_back(path);
+
+	ofstream out(directoryPath + path, ios::out | ios::binary);
+	if (!out) {
+		cout << "파일 열기 실패" << endl;
+		return;
+	}
+	
+	//파일 사이즈 받아오기
+	char file_size_ch[BUFSIZE];
+	ret = recv(client_sock, file_size_ch, BUFSIZE, 0);
+	temp = file_size_ch;
+	int file_size = stoi(temp);
+
+	char* file_data = new char[file_size];
+	
+	cout << "파일 사이즈 전달 완료 [" << file_size << "]" << endl;
+	send(client_sock, "1", 1, 0);
+	ret = recv(client_sock, file_data, file_size, 0);
+
+	out.write((char*)file_data, file_size);
+
+	send(client_sock, "1", 1, 0);
+	char addr[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+	cout << "[TCP 서버] " << addr << ":" <<
+		ntohs(clientaddr.sin_port) << "가 서버로 "<<path<<"를 전송했습니다."<<endl;
+	
+	out.close();
+	delete[] file_data;
+}
+
+void recv_file(SOCKET client_sock, sockaddr_in clientaddr) {
+	char addr[INET_ADDRSTRLEN];
+	char sign;
+	while (!WSAGetLastError()) {
+		char buf[BUFSIZE] = {};
+		string temp;
+		int ret = recv(client_sock, buf, 4, 0);
+		if (ret == 0 || ret == SOCKET_ERROR) return;
+
+		int command = buf[ret - 1] - '0';
+		int num = 0;
+
+		switch (command) {
+		case 1:
+			recv_file_clientToServer(client_sock, clientaddr);
+			break;
+		case 2:
+			temp = to_string(paths.size());
+			send(client_sock, temp.c_str(), temp.length() + 1, 0);
+			recv(client_sock, &sign, 1, 0);
+
+			for (int i = 0;i < paths.size();i++) {
+				temp = to_string(i + 1) + " : " + paths[i];
+				send(client_sock, temp.c_str(), temp.length() + 1, 0);
+				recv(client_sock, &sign, 1, 0);
+			}
+			ret = recv(client_sock, buf, 1024, 0);
+			send(client_sock, "1", 1, 0);
+			num = atoi(buf);
+			send_file(client_sock, num, clientaddr);
+			break;
+		case 3:
+			temp = to_string(paths.size());
+			send(client_sock, temp.c_str(), temp.length() + 1, 0);
+			recv(client_sock, &sign, 1, 0);
+			for (int i = 0;i < paths.size();i++) {
+				temp = to_string(i + 1) + " : " + paths[i];
+				send(client_sock, temp.c_str(), temp.length() + 1, 0);
+				recv(client_sock, &sign, 1, 0);
+			}
+			break;
+		case 4:
+			temp = to_string(paths.size());
+			send(client_sock, temp.c_str(), temp.length() + 1, 0);
+			recv(client_sock, &sign, 1, 0);
+
+			for (int i = 0;i < paths.size();i++) {
+				temp = to_string(i + 1) + " " + paths[i];
+				send(client_sock, temp.c_str(), temp.length() + 1, 0);
+				recv(client_sock, &sign, 1, 0);
+			}
+			ret = recv(client_sock, buf, BUFSIZE, 0);
+			send(client_sock, "1", 1, 0);
+			num = atoi(buf);
+			if (::remove((directoryPath + paths[num - 1]).c_str()) == 0) {
+				paths.erase(paths.begin() + (num - 1));
+				cout << "파일 삭제에 성공했습니다." << endl;
+			}
+			else {
+				cout << "파일 삭제에 실패했습니다." << endl;
+			}
+			break;
+		case 7:
+			inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+			closesocket(client_sock);
+			cout<<"[TCP 서버] 클라이언트 종료: IP 주소="<<addr<<", 포트 번호="<<
+				ntohs(clientaddr.sin_port)<<endl;
+			return;
+		default:
+			break;
+		}
+	}
+}
+int main()
+{	
+	init_paths();
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+	SOCKET server_sock{ socket(AF_INET, SOCK_STREAM, 0)};
+	if (server_sock == INVALID_SOCKET) return 1;
+
+	sockaddr_in sock_addr{ AF_INET, htons(4444) };
+	sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if (::bind(server_sock, (sockaddr*)&sock_addr, sizeof(sock_addr)) == SOCKET_ERROR) return 1;
+	if (listen(server_sock, SOMAXCONN) == SOCKET_ERROR) return 1;
+
+	SOCKET client_sock;
+	sockaddr_in clientaddr;
+	int addrlen;
+
+	addrlen = sizeof(clientaddr);
+	client_sock = accept(server_sock, (sockaddr*)&clientaddr, &addrlen);
+	if (client_sock == INVALID_SOCKET) return 1;
+	
+	char addr[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+	cout << endl << "[TCP 서버] 클라이언트 접속 : IP 주소=" << addr
+		<< ", 포트 번호=" << ntohs(clientaddr.sin_port) << endl;
+
+	thread th(recv_file, client_sock, clientaddr);
+
+	bool serverFlag = true;
+	while (serverFlag) {
+		int command;
+		cout << "1: 파일 리스트 출력" << endl;
+		cout << "2: 파일 삭제" << endl;
+		cout << "3: 서버 종료" << endl;
+		cin >> command;
+
+		if (command == 1) {
+			for (int i = 0;i < paths.size();i++) {
+				cout << i + 1 << " : " << paths[i] << endl;
+			}
+		}
+		else if (command == 2) {
+			int num;
+			if (paths.empty()) {
+				cout << "삭제할 파일이 없습니다." << endl;
+				continue;
+			}
+
+			for (int i = 0;i < paths.size();i++) {
+				cout << i + 1 << " : " << paths[i] << endl;
+			}
+			
+			cout << "몇 번 파일을 삭제할건가요? : ";
+			cin >> num;
+			if (num <= 0 && num > paths.size()) {
+				cout << "없는 파일 번호를 선택했습니다." << endl;
+				continue;
+			}
+			if (::remove((directoryPath + paths[num - 1]).c_str()) == 0) {
+				paths.erase(paths.begin() + (num - 1));
+				cout << "파일 삭제에 성공했습니다." << endl;
+			}
+			else {
+				cout << "파일 삭제에 실패했습니다." << endl;
+			}
+		}
+		else if (command == 3) {
+			break;
+		}
+	}
+
+	th.join();
+
+	closesocket(server_sock);
+
+	WSACleanup();
+	return 0;
+}
